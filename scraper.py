9cc800b491b04fb00c951805d57a34d6abfcce25
varungolusupudi <varungolusupudi@gmail.com>
2024-01-11 15:25:04 -0800

    Initial commit with scraper script

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Initialize Selenium WebDriver
+# Initialize Selenium WebDrivers
 driver = webdriver.Chrome()
 
 # URL of the UCSC nutrition page
